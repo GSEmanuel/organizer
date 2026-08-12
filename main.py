c41d1b632ecbc97ce7245b115f80d9b8479bb6f1
Gonzalez Santiago <gsemanuel@yahoo.com>
2026-08-12 10:29:38 -0300

    agregado botonera izquierda

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 from recursos import *
 from recursos.menu_bar import * # importa MenuBar
-
+# from recursos.frame_ventana import * # importa FramePrincipal
 class MainApp(Tk):
 	def __init__(self):
 		super(MainApp,self).__init__()
@@ -22,10 +22,37 @@ class MainApp(Tk):
 		self.bind('<Control-Q>', lambda x:self.destroy())
 
 		# inicializamos el loop principal de la ventana
+
+		# self.var_ruta_carpeta = StringVar()
+		# ruta_carpeta = ttk.Entry(self, state= 'readonly',)
+
+		# ruta_carpeta.grid(row = 0, column = 0)
+
+		boton_principal = ttk.Button(self, text='Principal')
+		boton_principal.grid(row = 0, column = 0, sticky = 'ew')
+
+		boton_comparar = ttk.Button(self, text = 'Comparar/fusionar')
+		boton_comparar.grid(row = 1, column = 0, sticky = 'ew')
+
+		boton_respaldos = ttk.Button(self, text = 'Respaldos')
+		boton_respaldos.grid(row = 2, column = 0, sticky = 'ew')
+
+		# self.columnconfigure(0, weight = 1)
+		self.columnconfigure(1, weight = 1)
+		self.columnconfigure(2, weight = 1)
+
+		self.rowconfigure(0, weight = 1)
+		self.rowconfigure(1, weight = 1)
+		self.rowconfigure(2, weight = 1)
+
 		self.mainloop()
+
 
 	def mostrar_ayuda(self):
 		Ayuda()
+	
+	def mostrar_frame(self, frame):
+		frame.tkraise()
 		
 
 	
---
+++ recursos/frame_ventana.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+class FramePrincipal(Frame):
+    def __init__(self,):
+        super(FramePrincipal, self).__init__()
+        
